2bf57b59b8ce2efe8a1acdd175dccbcbba554c94
Yogesh Bisht <reinzire@gmail.com>
2024-04-25 12:42:22 +0530

    rename application name

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex flex-1 items-center justify-center">
-      <Text className="text-3xl font-pblack">RNLearnJSM!</Text>
+      <Text className="text-3xl font-pblack">VidUpyr!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile
